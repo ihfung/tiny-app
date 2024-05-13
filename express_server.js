45941186fe059eb277e676b6c77afbb3e463465e
ihleung <ihfungleung@gmail.com>
2024-05-13 19:33:13 -0400

    updated

--- express_server.js
+++ express_server.js
@@ -4,11 +4,14 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
-
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+function generateRandomString() {}
+
+app.use(express.urlencoded({ extended: true }));
 
 //req = request, res = response
 app.get("/", (req, res) => {
@@ -28,10 +31,20 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
